597e943cff344b7079e2a3ad650f59609242abc0
Craig Burton <the.number.zero@gmail.com>
2013-05-06 00:00:21 -0400

    Hero unit v1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,45 @@
 module ApplicationHelper
 
+  # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Craig Burton"
     if page_title.empty?
-      base_title
+      return "#{base_title}, Developer"
     else
-      "#{base_title} | #{page_title}"
+      return "#{base_title} | #{page_title}"
     end
   end
 
   def nav_link(link_text, link_path)
+    #figure out if this links to the current page
     class_name = current_page?(link_path) ? 'active' : ''
 
     content_tag(:li, :class => class_name) do
       if class_name == '' then
         link_to link_text, link_path
       else
+        # just link to the top instead. No need for a fresh
+        # set of HTTP requests. =)
         link_to link_text, '#'
       end
     end
+  end
+
+  def button_link(button_class, link_text, link_path)
+    # clearly we want a button.
+    classes = "btn"
+
+    # split the classes passed in for parsing
+    class_arr = button_class.split(" ")
+
+    class_arr.each do |c|
+      # make sure we aren't going to end up with btn-pull-left or btn-btn-whatever
+      if not c[0..3].eql? "pull"  and not c[0..2].eql? "btn" then
+        c = "btn-" + c
+      end
+      classes = classes + " " + c
+    end
+    link_to link_text, link_path, { class: classes }
   end
 
 end
